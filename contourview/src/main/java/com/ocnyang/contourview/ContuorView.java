6b8731609b49f40d4cde59b3ccb4c7285860df99
OCN.Yang <yangocn@163.com>
2017-08-24 18:02:54 +0800

    Initial commit

---
+++ contourview/src/main/java/com/ocnyang/contourview/ContuorView.java
@@ -0,0 +1,357 @@
+package com.ocnyang.contourview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.graphics.SweepGradient;
+import android.support.annotation.ColorInt;
+import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*******************************************************************
+ *    * * * *   * * * *   *     *       Created by OCN.Yang
+ *    *     *   *         * *   *       Time:2017/8/23 09:34.
+ *    *     *   *         *   * *       Email address:ocnyang@gmail.com
+ *    * * * *   * * * *   *     *.Yang  Web site:www.ocnyang.com
+ *******************************************************************/
+
+
+public class ContuorView extends View {
+
+    private final static int SHADER_MODE_NULL = 0x00;
+    private final static int SHADER_MODE_RADIAL = 0x01;
+    private final static int SHADER_MODE_SWEEP = 0x02;
+    private final static int SHADER_MODE_LINEAR = 0x03;
+    private final static int SHADER_MODE_CUSTOM = 0x04;
+    private int mW;
+    private int mH;
+    private int mShaderColor;
+
+    @IntDef({SHADER_MODE_NULL, SHADER_MODE_RADIAL, SHADER_MODE_SWEEP, SHADER_MODE_LINEAR})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface ShaderMode {
+    }
+
+    private final static int SHADER_STYLE_LEFT_TO_BOTTOM = 0x00;
+    private final static int SHADER_STYLE_RIGHT_TO_BOTTOM = 0x11;
+    private final static int SHADER_STYLE_TOP_TO_BOTTOM = 0x12;
+    private final static int SHADER_STYLE_CENTER = 0x13;
+
+    @IntDef({SHADER_STYLE_LEFT_TO_BOTTOM, SHADER_STYLE_RIGHT_TO_BOTTOM, SHADER_STYLE_TOP_TO_BOTTOM, SHADER_STYLE_CENTER})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface ShaderStyle {
+    }
+
+    public final static int STYLE_SAND = 0x00;
+    public final static int STYLE_CLOUDS = 0x21;
+    public final static int STYLE_RIPPLES = 0x22;
+    public final static int STYLE_BEACH = 0x23;
+    public final static int STYLE_SHELL = 0x25;
+    private final static int STYLE_NULL = 0x24;
+
+    @IntDef({STYLE_SAND, STYLE_CLOUDS, STYLE_RIPPLES, STYLE_BEACH,STYLE_SHELL})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Style {
+    }
+
+    public final static float SMOOTHNESS_DEF = 0.25F;
+    //近似等价于 平滑系数，0~1，越小越平滑但是锚点的角度越大，越大越弯曲但是锚点附近的角度越平滑，建议：0.15~0.3
+    private float mSmoothness = 0.25F;
+
+    //填充方式
+    private int mShaderMode;
+    //填充开始颜色
+    private int mShaderStartColor;
+    //填充结束颜色
+    private int mShaderEndColor;
+    //填充的方向，起始点的控制
+    private int mShaderStyle;
+    //自带的填充样式
+    private int mStyle;
+    //填充
+    private Shader[] mShader;
+    //List<> 轮廓图个数，Point[] 锚点个数
+    private List<Point[]> mPointsList;
+    private Paint mPaint;
+
+    public ContuorView(Context context) {
+        this(context, null);
+    }
+
+    public ContuorView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ContuorView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.FILL);
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ContuorView);
+        mStyle = typedArray.getInt(R.styleable.ContuorView_contuor_style, STYLE_SAND);
+
+        float smn = typedArray.getFloat(R.styleable.ContuorView_smoothness, SMOOTHNESS_DEF);
+        if (smn <= 0) {
+            mSmoothness = 0.1F;
+        } else if (smn >= 1) {
+            mSmoothness = 0.99F;
+        } else {
+            mSmoothness = smn;
+        }
+
+        int shader_mode = typedArray.getInt(R.styleable.ContuorView_shader_mode, SHADER_MODE_NULL);
+        mShaderMode = shader_mode;
+        if (SHADER_MODE_NULL != shader_mode) {
+            mShaderStartColor = typedArray.getColor(R.styleable.ContuorView_shader_startcolor, Color.argb(90, 255, 255, 255));
+            mShaderEndColor = typedArray.getColor(R.styleable.ContuorView_shader_endcolor, Color.argb(90, 255, 255, 255));
+            mShaderStyle = typedArray.getInt(R.styleable.ContuorView_shader_style, SHADER_STYLE_LEFT_TO_BOTTOM);
+        } else {
+            mShaderColor = typedArray.getColor(R.styleable.ContuorView_shader_color, Color.argb(90, 255, 255, 255));
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mW = w;
+        mH = h;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        boolean haveShader = false;
+        if (mShaderMode == SHADER_MODE_NULL) {
+            mPaint.setColor(mShaderColor);
+        } else {
+            haveShader = true;
+        }
+
+        if (mStyle != STYLE_NULL) {
+            mPointsList = PointsFactory.getPoints(mStyle, mW, mH);
+        }
+        int flag = 0;
+        drawcontuor:
+        for (Point[] pts : mPointsList) {
+            flag++;
+            int length = pts.length;
+            if (length < 4) {
+                continue drawcontuor;
+            }
+            Path path = new Path();
+            int x_min = 0, y_min = 0, x_max = 0, y_max = 0;
+            for (int i = 0; i < length; i++) {
+                Point p_i, p_1i, p_i1, p_i2;
+                // Point ai,bi;
+                float ai_x, ai_y, bi_x, bi_y;
+
+                p_i = pts[i];
+                if (i == 0) {
+                    path.moveTo(pts[0].x, pts[0].y);
+                    x_max = x_min = pts[0].x;
+                    y_min = y_max = pts[0].y;
+                    p_1i = pts[length - 1];
+                    p_i1 = pts[i + 1];
+                    p_i2 = pts[i + 2];
+                } else if (i == length - 1) {
+                    p_1i = pts[i - 1];
+                    p_i1 = pts[0];
+                    p_i2 = pts[1];
+                } else if (i == length - 2) {
+                    p_1i = pts[i - 1];
+                    p_i1 = pts[i + 1];
+                    p_i2 = pts[0];
+                } else {
+                    p_1i = pts[i - 1];
+                    p_i1 = pts[i + 1];
+                    p_i2 = pts[i + 2];
+                }
+
+                if (p_1i == null || p_i == null || p_i1 == null || p_i2 == null) {
+                    continue drawcontuor;
+                }
+
+                ai_x = p_i.x + (p_i1.x - p_1i.x) * mSmoothness;
+                ai_y = p_i.y + (p_i1.y - p_1i.y) * mSmoothness;
+
+                bi_x = p_i1.x - (p_i2.x - p_i.x) * mSmoothness;
+                bi_y = p_i1.y - (p_i2.y - p_i.y) * mSmoothness;
+
+                path.cubicTo(ai_x, ai_y, bi_x, bi_y, p_i1.x, p_i1.y);
+
+                if (pts[i].x < x_min) {
+                    x_min = pts[i].x;
+                }
+                if (pts[i].x > x_max) {
+                    x_max = pts[i].x;
+                }
+                if (pts[i].y < y_min) {
+                    y_min = pts[i].y;
+                }
+                if (pts[i].y > y_max) {
+                    y_max = pts[i].y;
+                }
+            }
+
+            if (haveShader) {
+                if (mShaderMode == SHADER_MODE_CUSTOM && mShader != null) {
+                    mPaint.setShader(mShader[flag % mShader.length]);
+                } else {
+                    Point startPoint, endPoint;
+                    switch (mShaderStyle) {
+                        case SHADER_STYLE_LEFT_TO_BOTTOM:
+                            startPoint = new Point(x_min, y_min);
+                            endPoint = new Point(x_max, y_max);
+                            break;
+                        case SHADER_STYLE_RIGHT_TO_BOTTOM:
+                            startPoint = new Point(x_max, y_min);
+                            endPoint = new Point(x_min, y_max);
+                            break;
+                        case SHADER_STYLE_TOP_TO_BOTTOM:
+                            startPoint = new Point((x_max - x_min) / 2 + x_min, y_min);
+                            endPoint = new Point(x_min, (y_max - y_min) / 2 + y_min);
+                            break;
+                        case SHADER_STYLE_CENTER:
+                            startPoint = new Point((x_max - x_min) / 2 + x_min, (y_max - y_min) / 2 + y_min);
+                            endPoint = new Point(x_max, (y_max - y_min) / 2 + y_min);
+                            break;
+                        default:
+                            startPoint = new Point(0, 0);
+                            endPoint = new Point(mW, mH);
+                            break;
+                    }
+
+                    switch (mShaderMode) {
+                        case SHADER_MODE_RADIAL:
+                            RadialGradient radialGradient = new RadialGradient(
+                                    startPoint.x, endPoint.y,
+                                    (int) (Math.sqrt(Math.pow(Math.abs(x_max - x_min), 2) + Math.pow((Math.abs(y_max - y_min)), 2))),
+                                    mShaderStartColor,
+                                    mShaderEndColor,
+                                    Shader.TileMode.CLAMP);
+                            mPaint.setShader(radialGradient);
+                            break;
+                        case SHADER_MODE_SWEEP:
+                            SweepGradient sweepGradient = new SweepGradient(startPoint.x, startPoint.y, mShaderStartColor, mShaderEndColor);
+                            mPaint.setShader(sweepGradient);
+                            break;
+                        case SHADER_MODE_LINEAR:
+                            LinearGradient linearGradient = new LinearGradient(startPoint.x, startPoint.y, endPoint.x, endPoint.y,
+                                    mShaderStartColor, mShaderEndColor,
+                                    Shader.TileMode.REPEAT);
+                            mPaint.setShader(linearGradient);
+                            break;
+                    }
+                }
+            }
+            canvas.drawPath(path, mPaint);
+        }
+    }
+
+
+    //----------get or set---------------------------------------------------------------------------
+
+    public void setPoints(Point... points) {
+        mPointsList = new ArrayList<>(1);
+        mPointsList.add(points);
+        mStyle = STYLE_NULL;
+    }
+
+    public void setPoints(int... pts) {
+        this.setPoints(ptsArrTopoints(pts));
+    }
+
+    public void setPoints(Point[]... pointsArr) {
+        mPointsList = Arrays.asList(pointsArr);
+        mStyle = STYLE_NULL;
+    }
+
+    public void setPoints(int[]... ptsArr) {
+        mPointsList = new ArrayList<>();
+        for (int[] pts : ptsArr) {
+            mPointsList.add(ptsArrTopoints(pts));
+        }
+        mStyle = STYLE_NULL;
+    }
+
+    public Point[] ptsArrTopoints(int... pts) {
+        Point[] points = new Point[(pts.length) % 2];
+        for (int i = 0, j = 0; i < pts.length && j < points.length; i += 2, j++) {
+            points[j] = new Point(pts[i], pts[i + 1]);
+        }
+        return points;
+    }
+
+    public int getShaderMode() {
+        return mShaderMode;
+    }
+
+    public void setShaderMode(@ShaderMode int shaderMode) {
+        mShaderMode = shaderMode;
+    }
+
+    public int getShaderStartColor() {
+        return mShaderStartColor;
+    }
+
+    public void setShaderStartColor(@ColorInt int shaderStartColor) {
+        mShaderStartColor = shaderStartColor;
+    }
+
+    public int getShaderEndColor() {
+        return mShaderEndColor;
+    }
+
+    public void setShaderEndColor(@ColorInt int shaderEndColor) {
+        mShaderEndColor = shaderEndColor;
+    }
+
+    public int getShaderStyle() {
+        return mShaderStyle;
+    }
+
+    public void setShaderStyle(@ShaderStyle int shaderStyle) {
+        mShaderStyle = shaderStyle;
+    }
+
+    public int getStyle() {
+        return mStyle;
+    }
+
+    public void setStyle(@Style int style) {
+        mStyle = style;
+    }
+
+    public Shader[] getShader() {
+        return mShader;
+    }
+
+    public void setShader(Shader... shader) {
+        mShader = shader;
+    }
+
+    public float getSmoothness() {
+        return mSmoothness;
+    }
+
+    public void setSmoothness(float smoothness) {
+        mSmoothness = smoothness;
+    }
+    //    invalidate();
+}
---
+++ contourview/src/main/java/com/ocnyang/contourview/PointsFactory.java
@@ -0,0 +1,212 @@
+package com.ocnyang.contourview;
+
+import android.graphics.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.ocnyang.contourview.ContuorView.STYLE_BEACH;
+import static com.ocnyang.contourview.ContuorView.STYLE_CLOUDS;
+import static com.ocnyang.contourview.ContuorView.STYLE_RIPPLES;
+import static com.ocnyang.contourview.ContuorView.STYLE_SAND;
+import static com.ocnyang.contourview.ContuorView.STYLE_SHELL;
+
+/*******************************************************************
+ *    * * * *   * * * *   *     *       Created by OCN.Yang
+ *    *     *   *         * *   *       Time:2017/8/23 14:53.
+ *    *     *   *         *   * *       Email address:ocnyang@gmail.com
+ *    * * * *   * * * *   *     *.Yang  Web site:www.ocnyang.com
+ *******************************************************************/
+
+
+public final class PointsFactory {
+    public static List<Point[]> getPoints(@ContuorView.Style int style, int width, int hight) {
+        switch (style) {
+            case STYLE_SAND:
+                return getStyleSandPoints(width, hight);
+            case STYLE_BEACH:
+                return getStyleBeachPoints(width, hight);
+            case STYLE_CLOUDS:
+                return getStyleCloudsPoints(width, hight);
+            case STYLE_RIPPLES:
+                return getStyleRipplesPoints(width, hight);
+            case STYLE_SHELL:
+                return getStyleShellPoints(width, hight);
+            default:
+                return null;
+        }
+    }
+
+    public static List<Point[]> getStyleSandPoints(int width, int hight) {
+        List<Point[]> points = new ArrayList<>(2);
+        Point[] pts1 = new Point[4];
+        Point[] pts2 = new Point[4];
+        pts1[0] = new Point(0, hight / 6);
+        pts1[1] = new Point((int) (width * 0.9), hight / 5);
+        pts1[2] = new Point(width / 2, (int) (hight * (5.0 / 6)));
+        pts1[3] = new Point(0, (int) (hight * 0.75));
+
+        pts2[0] = new Point(width / 3, 0);
+        pts2[1] = new Point(width / 2, hight / 3);
+        pts2[2] = new Point(width, (int) (hight * 0.4));
+        pts2[3] = new Point(width, 0);
+
+        points.add(pts1);
+        points.add(pts2);
+        return points;
+    }
+
+    public static List<Point[]> getStyleBeachPoints(int width, int hight) {
+        List<Point[]> points = new ArrayList<>(2);
+        if (width > hight) {
+            Point[] pts1 = new Point[7];
+            Point[] pts2 = new Point[7];
+            pts1[0] = new Point((int) (width * 0.25), 0);
+            pts1[1] = new Point((int) (width * 0.166666667), (int) (hight * 0.25));
+            pts1[2] = new Point(width / 3, (int) (hight * 0.375));
+            pts1[3] = new Point((int) (width * 0.416666667), (int) (hight * 0.625));
+            pts1[4] = new Point((int) (width * 0.666666667), (int) (hight * 0.875));
+            pts1[5] = new Point(width, (int) (hight * 0.75));
+            pts1[6] = new Point(width, 0);
+
+            int distance = (int) (width * 0.05);
+            for (int i = 0; i < pts1.length; i++) {
+                if (i == 0 || i == pts1.length - 2) {
+                    pts2[i] = new Point(pts1[i].x + (i == 0 ? distance : 0), pts1[i].y - (i == pts1.length - 2 ? distance : 0));
+                } else {
+                    pts2[i] = new Point(pts1[i].x + distance, pts1[i].y - distance);
+                }
+            }
+            points.add(pts1);
+            points.add(pts2);
+            return points;
+        } else {
+            Point[] pts1 = new Point[8];
+            Point[] pts2 = new Point[8];
+            pts1[0] = new Point(0, (int) (hight * 0.75));
+            pts1[1] = new Point((int) (width * 0.25), (int) (hight * 0.83333333));
+            pts1[2] = new Point((int) (width * 0.375), (int) (hight * 0.666666667));
+            pts1[3] = new Point((int) (width * 0.625), (int) (hight * 0.583333333));
+            pts1[4] = new Point((int) (width * 0.875), (int) (hight * 0.333333333));
+            pts1[5] = new Point(width, (int) (hight * 0.0833333333));
+            pts1[6] = new Point(width, 0);
+            pts1[7] = new Point(0, 0);
+
+            int distance = (int) (width * 0.05);
+            for (int i = 0; i < pts1.length; i++) {
+                if (i == pts1.length - 2 || i == pts1.length - 1) {
+                    pts2[i] = pts1[i];
+                } else {
+                    pts2[i] = new Point(pts1[i].x + (i == 0 ? 0 : distance), pts1[i].y + distance);
+                }
+            }
+            points.add(pts1);
+            points.add(pts2);
+            return points;
+        }
+    }
+
+    public static List<Point[]> getStyleCloudsPoints(int width, int hight) {
+        List<Point[]> points = new ArrayList<>(2);
+        int high = width < hight ? width : hight;
+        Point[] pts1 = new Point[4];
+        Point[] pts2 = new Point[4];
+        Point[] pts3 = new Point[4];
+
+        pts1[0] = new Point(0, 0);
+        pts1[1] = new Point(0, (int) (high * 0.45));
+        pts1[2] = new Point((int) (width * 0.916666667), 0);
+        pts1[3] = new Point(0, 0);
+
+        pts2[0] = new Point(0, 0);
+        pts2[1] = new Point(0, (int) (high * 0.25));
+        pts2[2] = new Point((int) (width * 0.75), 0);
+        pts2[3] = new Point(0, 0);
+
+        pts3[1] = new Point((int) (width * 0.58333333), 0);
+        pts3[2] = new Point(width, (int) (high * 0.333333));
+        pts3[0] = new Point(width, 0);
+        pts3[3] = new Point(width, 0);
+
+        points.add(pts1);
+        points.add(pts2);
+        points.add(pts3);
+        return points;
+    }
+
+    public static List<Point[]> getStyleRipplesPoints(int width, int hight) {
+        List<Point[]> points = new ArrayList<>(2);
+        Point[] pts1 = new Point[4];
+        Point[] pts2 = new Point[4];
+        Point[] pts3 = new Point[4];
+        Point[] pts4 = new Point[4];
+        Point[] pts5 = new Point[4];
+
+        int radius = width / 6;
+        pts1[0] = new Point(width / 2 - radius * 2, hight / 2);
+        pts1[1] = new Point(width / 2, hight / 2 + radius);
+        pts1[2] = new Point(width / 2 + radius * 2, hight / 2);
+        pts1[3] = new Point(width / 2, hight / 2 - radius);
+
+        int radius2 = width / 4;
+        pts2[0] = new Point(width / 2 - radius2 * 2, hight / 2);
+        pts2[1] = new Point(width / 2, hight / 2 + radius2);
+        pts2[2] = new Point(width / 2 + radius2 * 2, hight / 2);
+        pts2[3] = new Point(width / 2, hight / 2 - radius2);
+
+        int radius3 = width / 3;
+        pts3[0] = new Point(width / 2 - radius3 * 2, hight / 2);
+        pts3[1] = new Point(width / 2, hight / 2 + radius3);
+        pts3[2] = new Point(width / 2 + radius3 * 2, hight / 2);
+        pts3[3] = new Point(width / 2, hight / 2 - radius3);
+
+        int radius4 = width / 5;
+        pts4[0] = new Point(width / 2 - radius4 * 2, hight / 2);
+        pts4[1] = new Point(width / 2, hight / 2 + radius4);
+        pts4[2] = new Point(width / 2 + radius4 * 2, hight / 2);
+        pts4[3] = new Point(width / 2, hight / 2 - radius4);
+
+        int radius5 = width / 2;
+        pts5[0] = new Point(width / 2 - radius5 * 2, hight / 2);
+        pts5[1] = new Point(width / 2, hight / 2 + radius5);
+        pts5[2] = new Point(width / 2 + radius5 * 2, hight / 2);
+        pts5[3] = new Point(width / 2, hight / 2 - radius5);
+
+        points.add(pts5);
+        points.add(pts3);
+        points.add(pts2);
+        points.add(pts4);
+        points.add(pts1);
+        return points;
+    }
+
+    public static List<Point[]> getStyleShellPoints(int width, int hight) {
+        List<Point[]> points = new ArrayList<>(2);
+        Point[] pts1 = new Point[4];
+        Point[] pts2 = new Point[4];
+        Point[] pts3 = new Point[4];
+
+        int radius = width / 6;
+        pts1[0] = new Point(width / 2 - radius * 2, hight / 2);
+        pts1[1] = new Point(width / 2, hight / 2 + radius);
+        pts1[2] = new Point(width / 2 + radius * 2, hight / 2);
+        pts1[3] = new Point(width / 2, hight / 2 - radius);
+
+        int radius2 = width / 4;
+        pts2[0] = new Point(width / 2 - radius2 * 2, hight / 2);
+        pts2[1] = new Point(width / 2, hight / 2 + radius2);
+        pts2[2] = new Point(width / 2 + radius * 2, hight / 2);
+        pts2[3] = new Point(width / 2, hight / 2 - radius2);
+
+        int radius3 = width / 3;
+        pts3[0] = new Point(width / 2 - radius3 * 2, hight / 2);
+        pts3[1] = new Point(width / 2, hight / 2 + radius3);
+        pts3[2] = new Point(width / 2 + radius * 2, hight / 2);
+        pts3[3] = new Point(width / 2, hight / 2 - radius3);
+
+        points.add(pts3);
+        points.add(pts2);
+        points.add(pts1);
+        return points;
+    }
+}
